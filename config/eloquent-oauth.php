2c2d2ad5012a75959a73cba2b31fb49cd945a088
rob <oh.ya.rob@gmail.com>
2018-03-28 20:58:16 -0500

    beginning of forum

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => '4d60d38a31986dc5bc1f',
+            'client_secret' => '47cdc701aab0af03bf3c8b5774a08fbc3588c6be',
+            'redirect_uri' => 'http://localhost:8000/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
